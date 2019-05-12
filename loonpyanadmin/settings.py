2f7e345ce00f6849a2faa0e70fa5d4eed06a5317
soesoemaw.ppt@gmail.com <soesoemaw.ppt@gmail.com>
2019-05-12 14:51:11 +0630

    ssm

--- loonpyanadmin/settings.py
+++ loonpyanadmin/settings.py
@@ -135,5 +135,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'loonpyanadmin', 'static'),
                     )
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
--- loonpyanadmin/urls.py
+++ loonpyanadmin/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 from administration import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url('admin/', admin.site.urls),
@@ -25,3 +27,7 @@ urlpatterns = [
     url(r'^administration/', include('administration.urls')),
     url(r'^inventory/', include('inventory.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
